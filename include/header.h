9b0241c5b716568ce5f9d845d9da36a29aee14de
sunzhifu <sunzhifu@xiaomi.com>
2022-10-02 12:12:21 +0800

    makefile

---
+++ include/header.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+class Blah{
+    public:
+    inline void boo(){
+        std::cout<<"Boo!\n";
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,7 @@
+/*文件名为main.cpp*/
+#include "header.h"
+#include <iostream>
+int main(){
+        std::cout<<"hello,world\n";
+        return 0;
+}
